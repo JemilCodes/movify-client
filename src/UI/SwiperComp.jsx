b4ee73741e0b171ea8fedf164981d375bddda388
JemilCodes <jemiluishaqodoba@gmail.com>
2024-01-23 08:03:25 +0100

    deploy2

--- src/UI/SwiperComp.jsx
+++ src/UI/SwiperComp.jsx
@@ -46,7 +46,6 @@ export default function App({
       setCurrentMovie(data?.data);
       setRoute("details");
     } catch (error) {
-      setRoute("details");
       setFeedback("Server is not responding, try again");
     }
   };
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Swiper from "../../UI/SwiperComp";
+import { BsFillGridFill } from "react-icons/bs";
+import { MdSwipeRight } from "react-icons/md";
 
 const Home = ({
   noMovie,
@@ -14,26 +16,81 @@ const Home = ({
 }) => {
   if (document && document?.querySelector(".banner")) {
     document.querySelector(".banner").style.backgroundImage =
-      "url('/banner.jpg')";
+      "url('movify-client/banner.jpg')";
   }
+
+  const [view, setView] = useState(true);
+
+  const handleDetails = async (payload) => {
+    setFeedback("Checking...");
+    try {
+      const data = await axios.post(`${baseUrl}/getSingle`, {
+        Title: searchResult[payload].Title,
+      });
+      changeBanner(data?.data?.Poster);
+      setFeedback("Here are your search result");
+      setCurrentMovie(data?.data);
+      setRoute("details");
+    } catch (error) {
+      setFeedback("Server is not responding, try again");
+    }
+  };
+
   return (
     <div
-      className={` relative rounded-lg w-full lg:w-[50%] flex flex-col gap-4 items-center swiperCont p-2 ${
-        noMovie && "skeleton-loader"
-      } ${showQuery && "!z-[-1]"} ${
-        feedback === "Searching..." && "skeleton-loader"
-      } ${feedback === "Checking..." && "skeleton-loader"}`}
+      className={` relative w-full flex lg:w-[50%] flex-col gap-3 items-center ${
+        !view && "max-h-[350px]"
+      } ${showQuery && "!z-[-1]"} `}
     >
-      <p className=" text-white text-2xl text-center font-extrabold">
-        {feedback}
-      </p>
-      <Swiper
-        searchResult={searchResult}
-        setCurrentMovie={setCurrentMovie}
-        setRoute={setRoute}
-        setFeedback={setFeedback}
-        changeBanner={changeBanner}
-      />
+      <div
+        className={` relative ${
+          !view && "overflow-y-scroll"
+        } rounded-lg w-full  flex flex-col gap-4 items-center swiperCont p-2 ${
+          noMovie && "skeleton-loader"
+        }  ${feedback === "Searching..." && "skeleton-loader"} ${
+          feedback === "Checking..." && "skeleton-loader"
+        }`}
+      >
+        <p className=" text-white text-2xl text-center font-extrabold">
+          {feedback}
+        </p>
+        {view && (
+          <Swiper
+            searchResult={searchResult}
+            setCurrentMovie={setCurrentMovie}
+            setRoute={setRoute}
+            setFeedback={setFeedback}
+            changeBanner={changeBanner}
+          />
+        )}
+        {!view && (
+          <div className=" grid sm:grid-cols-3 grid-cols-2 w-[90%] h-[90%] sm:gap-4">
+            {searchResult.map((data, i) => {
+              return (
+                <div className=" flex flex-col items-center  gap-1" key={i}>
+                  <img
+                    src={`${data?.Poster}`}
+                    alt={`${data?.Title}`}
+                    className="w-[75%] h-[75%] cursor-pointer rounded-xl "
+                    onClick={() => handleDetails(i)}
+                  />
+                  <p className=" line-clamp-1 w-[75%] text-base text-center font-bold text-white">
+                    {data?.Title}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+      <div
+        onClick={() => setView((prev) => !prev)}
+        className=" self-end max-w-[150px] gap-2 cursor-pointer h-9 flex items-center justify-between"
+      >
+        <p className=" text-white">Change View to</p>
+        {view && <BsFillGridFill className=" text-white" />}
+        {!view && <MdSwipeRight className=" text-white" />}
+      </div>
     </div>
   );
 };
